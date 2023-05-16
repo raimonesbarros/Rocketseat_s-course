7595285bb0ae2536436ed8b2f17bab76588954d8
Raimones Barros <raimonesrg3@gmail.com>
2023-05-16 19:22:33 -0300

    Insert components in App and update style App

--- Desafios/01-fundamentos-reactjs-ts/src/App.tsx
+++ Desafios/01-fundamentos-reactjs-ts/src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
+import { Header } from './components/Header.tsx';
+import { NewTask } from './components/NewTask.tsx';
+import { Tasks } from './components/Tasks.tsx';
+
+import styles from './App.module.css';
 
 export function App() {
 
   return (
-    <>
-      
-    </>
+    <div className={styles.app}>
+      <Header />
+      <main>
+        <NewTask />
+        <Tasks />
+      </main>
+
+    </div>
   );
 };
