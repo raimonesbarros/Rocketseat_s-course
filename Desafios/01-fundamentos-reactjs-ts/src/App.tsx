13274be44ca77f44cebdcea0ec91a8a442f91ee6
Raimones Barros <raimonesrg3@gmail.com>
2023-05-17 21:15:01 -0300

    insert functionalities Add and Delete task

--- Desafios/01-fundamentos-reactjs-ts/src/App.tsx
+++ Desafios/01-fundamentos-reactjs-ts/src/App.tsx
@@ -1,8 +1,66 @@
-import { Tasks } from './components/Tasks.tsx';
+import { ChangeEvent, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Task } from './components/Task.tsx';
+import { EmptyTask } from './components/EmptyTask.tsx';
 
 import styles from './App.module.css';
 
+interface TaskType {
+  id: string
+  checked: boolean
+  content: string
+}
+
+let checkedQtty = 0;
+
 export function App() {
+  const [writeNewTask, setWriteNewTask] = useState('');
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  function handleWriteTask(event: ChangeEvent<HTMLInputElement>){
+    const value = event.target.value;
+    setWriteNewTask(value)
+  };
+
+  function renderStatus(){
+    let qtty = 0;
+    tasks.map(task => task.checked==true? qtty+=1: '')
+    checkedQtty = qtty;
+    if(tasks.length===0) return <span>0</span>
+    else return <span>{checkedQtty} de {tasks.length}</span>
+  };
+
+  function handleAddNewTask(){
+    const id = uuidv4()
+    const newTask = {
+      id: id,
+      checked: false,
+      content: writeNewTask
+    }
+
+    setTasks(preState => [...preState, newTask])
+    setWriteNewTask('')
+  };
+
+  function deleteTask(value: string | null | undefined){
+    console.log(value)
+    const tasksWithoutTaskToDelete = tasks.filter(task => task.id!=value)
+    console.log(tasksWithoutTaskToDelete)
+    setTasks(tasksWithoutTaskToDelete)
+  };
+
+  const isValidTask = writeNewTask.length<1;
+
+  function renderTask(){
+    if(tasks.length===0) return <EmptyTask />
+    else return tasks.map(task =>
+      <Task
+        key={task.id}
+        id={task.id}
+        content={task.content}
+        onDeleteTask={deleteTask}  
+      />)
+  }
 
   return (
     <div className={styles.app}>
@@ -15,15 +73,40 @@ export function App() {
           <input
             type="text"
             placeholder='Adicione uma nova tarefa'
+            onChange={handleWriteTask}
+            value={writeNewTask}
             required
           />
-          <button type='submit'>Criar
+          <button
+            type='submit'
+            onClick={handleAddNewTask}
+            disabled={isValidTask}
+          >Criar
             <img src="./src/assets/plus.svg"/>
           </button>
         </div>
-        <Tasks />
+        <div className={styles.tasks}>
+      <div className={styles.info}>
+        <div className={styles.created}>
+          <p>Tarefas criadas <span>{tasks.length}</span></p>
+        </div>
+        <div className={styles.done}>
+          <p>Concluidas
+              {
+                renderStatus()
+              }
+          </p>
+        </div>
+      </div>
+      <div className={styles.tasksList}>
+        {
+          renderTask()
+        }
+      </div>
+    </div>
       </main>
 
     </div>
   );
+
 };
--- Desafios/01-fundamentos-reactjs-ts/src/components/Task.tsx
+++ Desafios/01-fundamentos-reactjs-ts/src/components/Task.tsx
@@ -1,12 +1,29 @@
 import { Trash } from '@phosphor-icons/react';
 import styles from './Task.module.css';
 
-export function Task(){
+interface TaskProps {
+  id?: string
+  content: string
+  onDeleteTask: (value: string | null | undefined) => void
+}
+
+export function Task({ id, content, onDeleteTask }: TaskProps){
+  
+  function handleDeleteTask(event: any){
+    
+    const id = event.target.parentNode?.firstChild.textContent;
+    onDeleteTask(id)
+  }
+  
   return(
     <div className={styles.task}>
+      <span>{id}</span>
       <input type="checkbox" />
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-      <Trash className={styles.trash} />
+      <p>{content}</p>
+      <Trash
+        className={styles.trash}
+        onClick={handleDeleteTask}  
+      />
     </div>
   );
 
