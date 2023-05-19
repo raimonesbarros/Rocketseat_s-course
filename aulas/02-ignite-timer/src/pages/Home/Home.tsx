0e8e3890ee1b74c903fe885f3aa7100f9cdad399
Raimones Barros <raimonesrg3@gmail.com>
2023-05-18 21:16:57 -0300

    create Home component

---
+++ aulas/02-ignite-timer/src/pages/Home/Home.tsx
@@ -0,0 +1,58 @@
+import { Play } from "phosphor-react";
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from "./Home.styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-sugestions"
+            placeholder="Dê um nome para seu projeto"
+          />
+
+          <datalist id="task-sugestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            placeholder="00"
+            id="minutesAmount"
+            step={5}
+            min={5}
+            max={60}
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountDownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  );
+}
