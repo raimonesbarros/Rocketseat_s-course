468500d49ccf8ff7be6914ba959c723f1976d648
Raimones Barros <raimonesrg3@gmail.com>
2023-05-23 23:06:20 -0300

    feat: create Home component

--- Desafios/02--coffee-delivery/src/Router.tsx
+++ Desafios/02--coffee-delivery/src/Router.tsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { DefautLayout } from "./styles/layouts/Default";
+import { Home } from "./pages/Home";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefautLayout />}></Route>
+      <Route path="/" element={<DefautLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
