00b275ac1272fd4aa1575ffaa0d85f0e984f477c
Raimones Barros <raimonesrg3@gmail.com>
2023-05-25 19:26:26 -0300

    fix: style Checkout component

--- Desafios/02--coffee-delivery/src/pages/Checkout/styles.ts
+++ Desafios/02--coffee-delivery/src/pages/Checkout/styles.ts
@@ -1,6 +1,8 @@
 import { styled } from "styled-components";
 
 export const CheckoutStyles = styled.div`
+  margin-top: 2.5rem;
+
   .container {
     width: 100%;
     max-width: 70rem;
@@ -10,5 +12,27 @@ export const CheckoutStyles = styled.div`
     display: flex;
     justify-content: space-between;
     gap: 2rem;
+
+    .delivery {
+      width: 57.14%;
+      max-width: 40rem;
+    }
+
+    .orderCard {
+      width: 40%;
+      max-width: 28rem;
+    }
+
+    h2 {
+      font-family: 'Baloo 2', cursive;
+      font-weight: 700;
+      font-size: 1.125rem;
+
+      display: flex;
+      align-items: center;
+
+      color: ${props => props.theme.base800};
+      margin-bottom: 1rem;
+    }
   }
 `;
