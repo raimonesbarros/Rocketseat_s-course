ca27c23da1112f904227bc6ab3ff815faac53f78
Raimones Barros <raimonesrg3@gmail.com>
2023-05-25 19:22:52 -0300

    feat: create FormAddress component

---
+++ Desafios/02--coffee-delivery/src/pages/Checkout/components/Address/FormAddress.styles.ts
@@ -0,0 +1,61 @@
+import { styled } from "styled-components";
+
+export const FormAddressStyles = styled.div`
+  form {
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+
+    input {
+      width: 100%;
+
+      padding: 0.75rem;
+
+      border: 1px solid ${(props) => props.theme.base400};
+      border-radius: 4px;
+
+      background: ${(props) => props.theme.base300};
+      color: ${(props) => props.theme.base700};
+
+      &::placeholder {
+        font-size: 0.875rem;
+        color: ${(props) => props.theme.base600};
+      }
+    }
+
+    label[for="cep"],
+    label[for="number"],
+    label[for="district"] {
+      width: 35.7%;
+    }
+
+    label[for="street"] {
+      width: 100%;
+    }
+
+    label[for="complement"] {
+      width: 62.9%;
+    }
+
+    label[for="city"] {
+      width: 50.6%;
+    }
+
+    label[for="uf"] {
+      width: 10.714%;
+    }
+
+    .row3,
+    .row4 {
+      width: 100%;
+      display: flex;
+      column-gap: 0.5rem;
+    }
+
+    .row3 {
+      grid-template-columns: auto auto;
+    }
+  }
+`;
---
+++ Desafios/02--coffee-delivery/src/pages/Checkout/components/Address/FormAddress.tsx
@@ -0,0 +1,38 @@
+import { FormAddressStyles } from "./FormAddress.styles";
+
+export function FormAddress() {
+  return (
+    <FormAddressStyles>
+      <form action="">
+        <label htmlFor="cep">
+          <input type="text" id="cep" placeholder="CEP" />
+        </label>
+
+        <label htmlFor="street">
+          <input type="text" id="street" placeholder="Rua" />
+        </label>
+
+        <div className="row3">
+          <label htmlFor="number">
+            <input type="text" id="number" placeholder="Número" />
+          </label>
+          <label htmlFor="complement">
+            <input type="text" id="complement" placeholder="Complemento" />
+          </label>
+        </div>
+
+        <div className="row4">
+          <label htmlFor="district">
+            <input type="text" id="district" placeholder="Bairro" />
+          </label>
+          <label htmlFor="city">
+            <input type="text" id="city" placeholder="Cidade" />
+          </label>
+          <label htmlFor="uf">
+            <input type="text" id="uf" placeholder="UF" />
+          </label>
+        </div>
+      </form>
+    </FormAddressStyles>
+  );
+}
